b3bf8513d9c8979ff39ab28f62357fb346b7a04a
Albert Shaidullin <3litac@gmail.com>
2024-08-18 16:33:30 +0200

    💄 Bring lualine

---
+++ lua/albert/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+  opts = {
+    options = {
+      theme = 'gruvbox',
+      section_separators = '',
+      component_separators = '',
+    },
+  },
+}
