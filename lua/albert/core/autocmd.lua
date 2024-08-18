2cd35546509fc49782617991d04d2df68f77bc08
Albert Shaidullin <3litac@gmail.com>
2024-08-18 15:58:04 +0200

    🧑‍💻 Polish on save

--- lua/albert/core/autocmd.lua
+++ lua/albert/core/autocmd.lua
@@ -1,6 +1,20 @@
--- Restore cursor to the default when leaving Neovim
+vim.api.nvim_create_augroup("reset_cursor", { clear = true })
 vim.api.nvim_create_autocmd("VimLeave", {
+  desc = "Reset cursor to underscore when leaving editor",
+  group = "reset_cursor",
   callback = function()
-    vim.cmd([[set guicursor=a:hor90]])
+    vim.api.nvim_command('set guicursor=a:hor90')
   end,
+})
+
+vim.api.nvim_create_augroup("trim_on_save", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Trim trailing whitespaces",
+  group = "trim_on_save",
+  command = ":%s/\\s\\+$//e",
+})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Trim final newlines",
+  group = "trim_on_save",
+  command = ":%s/\\($\\n\\s*\\)\\+\\%$//e",
 })
