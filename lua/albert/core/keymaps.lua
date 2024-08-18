f54a3fb1a226fd6c572eb7d5ad60dcf86899e646
Albert Shaidullin <3litac@gmail.com>
2024-08-18 16:09:44 +0200

    🧑‍💻 Add a shorcut to auto-indent file

--- lua/albert/core/keymaps.lua
+++ lua/albert/core/keymaps.lua
@@ -5,3 +5,4 @@ local keymap = vim.keymap
 keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save File" })
 keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit File" })
 keymap.set('n', '<leader>uh', ':nohl<CR>', { desc = "Clear search highlights" })
+keymap.set('n', '<leader>=', 'ggVG=', { desc = "Select all and auto-indent" })
