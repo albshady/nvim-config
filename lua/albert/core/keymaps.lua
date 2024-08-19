52d3c0bd2451c54e5a0b77c1b3280f06a97f313e
Albert Shaidullin <3litac@gmail.com>
2024-08-19 11:15:33 +0200

    📝 Add descriptions to leader+l/g groups

--- lua/albert/core/keymaps.lua
+++ lua/albert/core/keymaps.lua
@@ -2,10 +2,13 @@ vim.g.mapleader = ' '
 
 local keymap = vim.keymap
 
-keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save File" })
-keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit File" })
+keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save file" })
+keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit file" })
 keymap.set('n', '<leader>uh', ':nohl<CR>', { desc = "Clear search highlights" })
 keymap.set('n', '<leader>=', 'ggVG=', { desc = "Select all and auto-indent" })
 keymap.set('n', '<leader>c', ':bd<CR>', { desc = "Close current buffer" })
 keymap.set('n', 'H', ':bprev<CR>', { desc = "Previous buffer" })
 keymap.set('n', 'L', ':bnext<CR>', { desc = "Next buffer" })
+
+keymap.set('n', '<leader>g', '', { desc = "Git commands" })
+keymap.set('n', '<leader>l', '', { desc = "Lsp actions" })
