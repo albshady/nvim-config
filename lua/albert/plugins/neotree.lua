db070154d40e5ad9b7af419b85a167725cf95117
Albert Shaidullin <3litac@gmail.com>
2024-09-20 15:52:16 +0200

    🧑‍💻 Use stylua pre-commit hook

--- lua/albert/plugins/neotree.lua
+++ lua/albert/plugins/neotree.lua
@@ -1,31 +1,31 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  keys = {
-    { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree" },
-    { "<leader>o", "<cmd>Neotree focus<CR>", desc = "Focus Neo-tree" },
-  },
-  opts = {
-    filesystem = {
-      follow_current_file = {
-        enabled = true, -- Automatically reveal the current file
-      },
-      filtered_items = {
-        hide_gitignored = true,
-        hide_dotfiles = false,
-        hide_by_name = {
-          "__pycache__",
-        },
-      },
-    },
-    window = {
-      width = 35,
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	keys = {
+		{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree" },
+		{ "<leader>o", "<cmd>Neotree focus<CR>", desc = "Focus Neo-tree" },
+	},
+	opts = {
+		filesystem = {
+			follow_current_file = {
+				enabled = true, -- Automatically reveal the current file
+			},
+			filtered_items = {
+				hide_gitignored = true,
+				hide_dotfiles = false,
+				hide_by_name = {
+					"__pycache__",
+				},
+			},
+		},
+		window = {
+			width = 35,
+		},
+	},
 }
--- lua/albert/plugins/telescope.lua
+++ lua/albert/plugins/telescope.lua
@@ -1,32 +1,44 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  keys = {
-    { "ff", function() require('telescope.builtin').find_files() end, desc = "Find Files" },
-    { "fw", function() require('telescope.builtin').live_grep() end, desc = "Live Grep" },
-  },
-  config = function()
-    local telescope = require('telescope')
-    local actions = require('telescope.actions')
+	keys = {
+		{
+			"ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"fw",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			desc = "Live Grep",
+		},
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
-  end,
+		telescope.load_extension("fzf")
+	end,
 }
